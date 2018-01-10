deed97be094a298660c67e35cdc14340e31165fb
subhendu <bhattasubhendu@gmail.com>
2018-01-10 20:41:15 +0545

    form displays

---
+++ resources/assets/js/components/FileUpload.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">VUe Example Component</div>
+
+                    <div class="panel-body">
+
+                        <form>
+                            <legend>Upload form</legend>
+
+                            <div class="form-group">
+                                <label>Upload Files</label>
+                                <input type="file" multiple class="form-control">
+                            </div>
+
+
+
+
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+<example></example>
+
+@endsection
